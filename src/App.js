65cc85616ff7d890936c4aab1ba19ddbe7c05c76
Chris Nyberg <chrisn83@gmail.com>
2022-06-15 15:25:39 -0400

    simple prototype wireframe created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Container } from 'react-bootstrap';
+import ProfileNavBar from './components/ProfileNavBar/profileNavBar';
+import ProfileBanner from './components/ProfileBanner/profileBanner';
+import ProfileContainer from './components/ProfileContainer/profileContainer';
+import ProfileFooter from './components/ProfileFooter/profileFooter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <Container id='appContainer'>
+      <ProfileNavBar />
+      <ProfileBanner />
+      <ProfileContainer />
+      <ProfileFooter />
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/components/ProfileBanner/profileBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ProfileBanner extends React.Component {
+    render() {
+        return (
+            <div className='banner'>
+                <h2>placeholder Banner</h2>
+                <p>placeholder</p>
+            </div>
+        )
+    }
+}
+
+export default ProfileBanner;
